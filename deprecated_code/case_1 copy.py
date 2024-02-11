fab5b093167b2c948a9db7847452b69dfb413ce6
Marcello Costamagna <marcello.costamagna@uib.no>
2024-02-11 01:19:17 +0100

    adds scripts in deprecated_code

---
+++ deprecated_code/case_1 copy.py
@@ -0,0 +1,44 @@
+# Scripts for calculating between all pairs of molecules in a list of molecules.
+
+import numpy as np  
+from nd_sim.pre_processing import *
+from nd_sim.pca_transform import * 
+from nd_sim.fingerprint import *
+from nd_sim.similarity import *
+from nd_sim.utils import *
+from trials.perturbations import *
+import os 
+
+np.set_printoptions(precision=4, suppress=True)
+
+cwd = os.getcwd()
+# PRE-PROCESSING
+# List of molecules from SDF file
+molecules = load_molecules_from_sdf(f'{cwd}/sd_data/optoiso_test/problematic_mols.sdf', removeHs=False, sanitize=False)
+
+### ROTATE MOLECULES ###
+rotated_molecules = []
+for molecule in molecules:
+    angle1 = np.random.randint(0, 360)
+    angle2 = np.random.randint(0, 360)
+    angle3 = np.random.randint(0, 360)
+    mol = rotate_molecule(molecule, angle1, angle2, angle3)
+    rotated_molecules.append(mol)
+
+
+fingerprints = [generate_fingerprint_from_molecule(molecule, features=None, scaling='matrix') for molecule in rotated_molecules]
+
+n_molecules = len(fingerprints)
+print(f'Shape Similarity:')
+for i in range(n_molecules-2):
+    for j in range(i+1, n_molecules):
+        similarity = compute_similarity_score(fingerprints[i], fingerprints[j])
+        print(f"{i+1}-{j+1}: {similarity:.4f}")
+        
+# fingerprints = [generate_fingerprint_from_molecule(molecule, features=DEFAULT_FEATURES, scaling='matrix') for molecule in rotated_molecules]
+
+# print(f'\nSimilarity with features:')
+# for i in range(n_molecules):
+#     for j in range(i+1, n_molecules):
+#         similarity = compute_similarity_score(fingerprints[i], fingerprints[j])
+#         print(f"{i+1}-{j+1}: {similarity:.4f}")
